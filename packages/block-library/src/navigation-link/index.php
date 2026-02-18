5ea58cedda5db6ccf1228320e4f4291ca1c0fff5
Ben Dwyer <ben@scruffian.com>
2026-02-18 17:35:21 +0000

    fix broken rebase

--- packages/block-library/src/navigation-link/index.php
+++ packages/block-library/src/navigation-link/index.php
@@ -89,17 +89,6 @@ function block_core_navigation_link_build_css_colors( $context, $attributes, $is
 }
 
 /**
- * Returns the top-level submenu SVG chevron icon.
- *
- * @since 5.9.0
- *
- * @return string
- */
-function block_core_navigation_link_render_submenu_icon() {
-	return '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none" aria-hidden="true" focusable="false"><path d="M1.50002 4L6.00002 8L10.5 4" stroke-width="1.5"></path></svg>';
-}
-
-/**
  * Decodes a url if it's encoded, returning the same url if not.
  *
  * @since 6.2.0
--- packages/block-library/src/navigation-submenu/index.php
+++ packages/block-library/src/navigation-submenu/index.php
@@ -58,17 +58,6 @@ if ( file_exists( __DIR__ . '/../navigation-link/shared/item-should-render.php' 
 }
 
 /**
- * Returns the top-level submenu SVG chevron icon.
- *
- * @since 5.9.0
- *
- * @return string
- */
-function block_core_navigation_submenu_render_submenu_icon() {
-	return '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none" aria-hidden="true" focusable="false"><path d="M1.50002 4L6.00002 8L10.5 4" stroke-width="1.5"></path></svg>';
-}
-
-/**
  * Renders the `core/navigation-submenu` block.
  *
  * @since 5.9.0
